7bbf4219c87c76d7dc7c87dbed2dd9b9a44c7a07
Vienna Scheyer <vienna.scheyer@gmail.com>
2021-05-10 16:01:58 -0400

    Update fsm test bench

--- layout/digital/openlane/verilog_test/fsm.t.v
+++ layout/digital/openlane/verilog_test/fsm.t.v
@@ -4,13 +4,12 @@
   if (val !== exp) $display("[FAIL] %s (got:b%b expected:b%b)", msg, val, exp);
 
 module testFSM();
-reg m_sense;
-reg l_sense;
-reg m_thresh_1; //higher threshold, less severe drought
-reg m_thresh_2; //lower threshold, indicates very dry
+reg [7:0] m_sense;
+reg [7:0] l_sense;
+reg [7:0] m_thresh_1; //higher threshold, less severe drought
+reg [7:0] m_thresh_2; //lower threshold, indicates very dry
 reg [1:0] l_thresh;
-reg water_time_short; //length of time to water when little dry in clk units
-reg water_time_long;  //length of time to water when very dry in clk units
+reg [7:0] water_time_in; //length of time to water when little dry in clk units
 reg clk;
 wire water_toggle;
 wire [1:0] state;
@@ -23,10 +22,8 @@ wire [1:0] state;
   fsm dut (.m_sense(m_sense),
             .l_sense(l_sense),
             .m_thresh_1(m_thresh_1),
-            .m_thresh_2(m_thresh_2),
             .l_thresh(l_thresh),
-            .water_time_short(water_time_short),
-            .water_time_long(water_time_long),
+            .water_time_in(water_time_in),
             .clk(clk),
             .water_toggle(water_toggle),
             .state(state));
@@ -37,44 +34,37 @@ wire [1:0] state;
 
   initial begin
   l_thresh <= 00; //this is the light reg to check for 01
-  m_sense <= 0; //start with moist soil
-  m_thresh_1 <= 2;
-  m_thresh_2 <= 1;
+  m_sense <= 8'd255; //start with moist soil
+  m_thresh_1 <= 8'd150;
+  m_thresh_2 <= 8'd100;
+  water_time_in = 8'd50;
   end
 
   initial begin
   $dumpfile("shiftreg-dump.vcd");
   $dumpvars();
   $display("FSM Test");
+  $display("Expected state | Actual state | Expected water output | Actual water output");
 
-  /* // test 1
-  $display("state: %b", state);
+  // Initial conditions
   @(posedge clk);
-  $display("state: %b", state);
+  $display("00             | %b           | 0                     | %b                 ", state, water_toggle);
+
+  // Change light sensor input
   l_thresh = 01;
   @(posedge clk)
-  // check that the state shifted
-    $display("water toggle: %b", water_toggle);
-    $display("state: %b", state);
-  `ASSERT_EQ(water_toggle, 0, "test 1");
-  */
+  $display("01             | %b           | 0                     | %b                 ", state, water_toggle);
 
-  $display("state: %b", state);
-  @(posedge clk);
-  $display("state: %b", state);
-  m_sense = 0.5;
+  // Change moisture sensor input
+  m_sense = 8'd50;
   @(posedge clk)
-  // check that the state shifted
-    $display("water toggle: %b", water_toggle);
-    $display("state: %b", state);
-  `ASSERT_EQ(water_toggle, 0, "test 1");
+  @(posedge clk)
+  $display("10             | %b           | 1                     | %b                 ", state, water_toggle);
 
-  @(posedge clk);
-  m_sense = 0;
-  @(posedge clk);
-  $display("water toggle: %b", water_toggle);
-  $display("state: %b", state);
-  `ASSERT_EQ(water_toggle, 0, "test 1");
+  //Wait for watering to happen
+  #510
+  $display("10             | %b           | 1                     | %b                 ", state, water_toggle);
+
 
   $finish();
 
