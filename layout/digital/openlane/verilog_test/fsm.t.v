f20df36684319d8cec9cdec096e991f1754f3cb2
Vienna Scheyer <vienna.scheyer@gmail.com>
2021-05-11 00:59:00 -0400

    change timing for fsm

--- layout/digital/openlane/verilog_test/fsm.t.v
+++ layout/digital/openlane/verilog_test/fsm.t.v
@@ -5,24 +5,14 @@
 
 module testFSM();
 reg [6:0] m_sense;
-reg [6:0] l_sense;
-reg [6:0] m_thresh_1; //higher threshold, less severe drought
-reg [6:0] m_thresh_2; //lower threshold, indicates very dry
-reg [1:0] l_thresh;
+reg [6:0] m_thresh; //higher threshold, less severe drought
 reg [6:0] water_time_in; //length of time to water when little dry in clk units
 reg clk;
 wire water_toggle;
 wire [1:0] state;
 
-  wire [1:0] MODE;
-  wire START;
-  wire misobuffCNTL;
-  wire [2:0] actualstate;
-
   fsm dut (.m_sense(m_sense),
-            .l_sense(l_sense),
-            .m_thresh_1(m_thresh_1),
-            .l_thresh(l_thresh),
+            .m_thresh(m_thresh),
             .water_time_in(water_time_in),
             .clk(clk),
             .water_toggle(water_toggle),
@@ -30,14 +20,12 @@ wire [1:0] state;
 
   // Generate clock
   initial clk=0;
-  always #1000000000 clk = !clk; //clk is in seconds
+  always #1 clk = !clk; //clk is in seconds
 
   initial begin
-  l_thresh <= 00; //this is the light reg to check for 01
   m_sense <= 7'd127; //start with moist soil
-  m_thresh_1 <= 7'd100;
-  m_thresh_2 <= 7'd50;
-  water_time_in = 7'd50;
+  m_thresh <= 7'd100;
+  water_time_in = 7'd3; //water for 5 minutes
   end
 
   integer i;
@@ -53,22 +41,16 @@ wire [1:0] state;
   $display("00             | %b           | 0                     | %b                 ", state, water_toggle);
 
   // Change light sensor input
-  l_thresh = 01;
-  @(posedge clk)
-  $display("01             | %b           | 0                     | %b                 ", state, water_toggle);
-
-  // Change moisture sensor input
-  m_sense = 8'd50;
-  @(posedge clk)
+  m_sense = 7'd90;
   @(posedge clk)
   $display("10             | %b           | 1                     | %b                 ", state, water_toggle);
 
-  // Reset moisture sensor
-  m_sense = 8'd255;
-  l_thresh = 00;
-  //Wait for watering to happen
-  #3000
-  $display("10             | %b           | 1                     | %b                 ", state, water_toggle);
+  // Wait for water to turn off
+  #10
+  @(posedge clk)
+  @(posedge clk)
+  $display("00             | %b           | 0                     | %b                 ", state, water_toggle);
+
 
 
   $finish();
--- layout/digital/openlane/verilog_test/fsm.v
+++ layout/digital/openlane/verilog_test/fsm.v
@@ -12,22 +12,14 @@ Count to 10 minutes for light sensor
 module fsm
 (
   input [6:0] m_sense,        // moisture sensor input from ADC
-  input [6:0] l_sense,        // light sensor input from ADC
-  input [6:0] m_thresh_1,     // moisture threshold from ADC
-  input [1:0] l_thresh,       // light threshold from ADC
+  input [6:0] m_thresh,     // moisture threshold from ADC
   input [6:0] water_time_in,  // length of time to water when little dry in clk units
   input clk,                  // clk in seconds from clk_divider
   output reg water_toggle,    // output of the system
   output reg [1:0] state      // keep track of fsm state
 );
 
-// reg [1:0] state;
-reg [3:0] light_sense_cnt;
 reg [6:0] water_time_cnt;
-reg [6:0] dawn_count;
-reg [6:0] dawn_timebox;       // length of time it's still considered dawn
-reg [6:0] m_thresh_2;
-reg l_sense_prev;
 
 initial begin
 
@@ -37,45 +29,25 @@ initial begin
   // Set initial Outputs
   water_toggle <= 0;
 
-  // Zero the counters
-  light_sense_cnt <= 4'b0;
+  // Zero the counter
   water_time_cnt <= 0;
 
-  // Set second moisture threshold as offset from first one
-  m_thresh_2 <= m_thresh_1 - 8'd50;
-
-  // Set light sensor to keep track of past light level
-  l_sense_prev = l_sense;
 end
 
 always @(posedge clk) begin
-  dawn_count += 1;
+
   water_time_cnt += 1;
-  if (state == `STANDBY && l_thresh == 01) begin
-    dawn_count = 0;
-    water_toggle = 0;
-    state = `STANDBY_DAWN;
-  end
-  if (state == `STANDBY && m_sense <= m_thresh_2) begin
-    water_time_cnt = 0;
+
+  if (state == `STANDBY && m_sense <= m_thresh) begin
     water_toggle = 1;
     state = `WATERING;
   end
-  if (state == `STANDBY_DAWN && m_sense <= m_thresh_1) begin
-    water_time_cnt = 0;
-    water_toggle = 1;
-    state = `WATERING;
-  end
-  if (state == `STANDBY_DAWN && dawn_count >= dawn_timebox) begin
+  if (state == `WATERING && water_time_cnt >= water_time_in) begin
     water_toggle = 0;
     state = `STANDBY;
   end
-  if (state == `WATERING && water_time_cnt >= water_time_in && dawn_count >= dawn_timebox) begin
-    state = `STANDBY;
-  end
-  if (state == `WATERING && water_time_cnt >= water_time_in && dawn_count < dawn_timebox) begin
-    state = `STANDBY_DAWN;
-  end
+
+
 end
 
 endmodule
